20da1ca94fd5fd20fbe5d5e20397f075fbae465d
mufeed khan <mufeedkhan192@gmail.com>
2021-12-23 23:39:15 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+// import './App.css'
+import Api from './Comonents/Api'
+import PostApi from './Comonents/PostApi'
+
+const App = () => {
+  return (
+    <div className='App'>
+     
+      <Api/>
+      <PostApi/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/UseRef.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const UseRef = () => {
+    return (
+        <div>
+           <input type></input>
+             
+        </div>
+    )
+}
+
+export default UseRef
+
+rafe
+
